4e78f1b5d794c54a5bdf29e883b83478d7e1c96a
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-08-19 23:34:51 +0500

    update lang

--- lang/ru/passwords.php
+++ lang/ru/passwords.php
@@ -3,20 +3,20 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
+    |------------------------------------------------- -------------------------
+    | Линии языка сброса пароля
+    |------------------------------------------------- -------------------------
     |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
+    | Следующие языковые строки являются строками по умолчанию, которые соответствуют причинам
+    | которые предоставляются брокером паролей для попытки обновления пароля
+    | произошел сбой, например из-за недопустимого токена или недопустимого нового пароля.
     |
-    */
+     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Ваш пароль был сброшен.',
+    'sent' => 'Мы отправили ссылку для сброса пароля по электронной почте.',
+    'throttled' => 'Пожалуйста, подождите, прежде чем повторить попытку.',
+    'token' => 'Этот токен сброса пароля недействителен.',
+    'user' => "Мы не можем найти пользователя с таким адресом электронной почты.",
 
 ];
