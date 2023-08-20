a6f97c2f06ea17f1e48b45ce6ee748ac6e92a1b1
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-08-20 15:46:44 +0500

    add attachs lang

---
+++ lang/en/attachs.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'header' => 'Documents',
+    'description' => 'Registration will close on 16 September 2023 at 17 hours 59 minutes or upon reaching the limit of allocated slots for registration for separate distances.',
+    'first' => 'Regulations',
+    'second' => 'Public offer',
+    'third' => 'Trace Certificate',
+    'fourth' => 'Health receipt',
+    'download' => 'Download',
+];
--- resources/js/Components/Attachs.vue
+++ resources/js/Components/Attachs.vue
@@ -5,28 +5,24 @@
                 <h2
                     class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
                 >
-                    Документы
+                    {{ __("attachs.header") }}
                 </h2>
                 <p class="mt-2 text-lg leading-8 text-gray-600">
-                    Регистрация участников завершится 16 сентября 2023 в 17
-                    часов 59 минут или по достижению лимита выделенных слотов
-                    для регистрации на отдельные дистанции.
+                    {{ __("attachs.description") }}
                 </p>
             </div>
             <div
                 class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-4"
             >
                 <article
-                    v-for="item in attachs"
-                    :key="item.id"
                     class="flex max-w-xl flex-col items-start justify-between"
                 >
                     <a
-                        :href="item.link"
+                        href="/reglament.pdf"
                         class="relative mt-8 flex items-center gap-x-4 hover:bg-slate-50 transition-colors duration-100 p-4 rounded-md cursor-pointer"
                     >
                         <img
-                            :src="item.imageUrl"
+                            :src="fileImg"
                             alt=""
                             class="h-10 w-10 bg-gray-50"
                         />
@@ -34,11 +30,86 @@
                             <div class="font-semibold text-gray-900">
                                 <p>
                                     <span class="absolute inset-0" />
-                                    {{ item.name }}
+                                    {{ __("attachs.first") }}
                                 </p>
                             </div>
                             <p class="text-blue-500 underline cursor-pointer">
-                                Скачать
+                                {{ __("attachs.download") }}
+                            </p>
+                        </div>
+                    </a>
+                </article>
+                <article
+                    class="flex max-w-xl flex-col items-start justify-between"
+                >
+                    <a
+                        href="/reglament.pdf"
+                        class="relative mt-8 flex items-center gap-x-4 hover:bg-slate-50 transition-colors duration-100 p-4 rounded-md cursor-pointer"
+                    >
+                        <img
+                            :src="fileImg"
+                            alt=""
+                            class="h-10 w-10 bg-gray-50"
+                        />
+                        <div class="text-sm leading-6">
+                            <div class="font-semibold text-gray-900">
+                                <p>
+                                    <span class="absolute inset-0" />
+                                    {{ __("attachs.second") }}
+                                </p>
+                            </div>
+                            <p class="text-blue-500 underline cursor-pointer">
+                                {{ __("attachs.download") }}
+                            </p>
+                        </div>
+                    </a>
+                </article>
+                <article
+                    class="flex max-w-xl flex-col items-start justify-between"
+                >
+                    <a
+                        href="/reglament.pdf"
+                        class="relative mt-8 flex items-center gap-x-4 hover:bg-slate-50 transition-colors duration-100 p-4 rounded-md cursor-pointer"
+                    >
+                        <img
+                            :src="fileImg"
+                            alt=""
+                            class="h-10 w-10 bg-gray-50"
+                        />
+                        <div class="text-sm leading-6">
+                            <div class="font-semibold text-gray-900">
+                                <p>
+                                    <span class="absolute inset-0" />
+                                    {{ __("attachs.third") }}
+                                </p>
+                            </div>
+                            <p class="text-blue-500 underline cursor-pointer">
+                                {{ __("attachs.download") }}
+                            </p>
+                        </div>
+                    </a>
+                </article>
+                <article
+                    class="flex max-w-xl flex-col items-start justify-between"
+                >
+                    <a
+                        href="/reglament.pdf"
+                        class="relative mt-8 flex items-center gap-x-4 hover:bg-slate-50 transition-colors duration-100 p-4 rounded-md cursor-pointer"
+                    >
+                        <img
+                            :src="fileImg"
+                            alt=""
+                            class="h-10 w-10 bg-gray-50"
+                        />
+                        <div class="text-sm leading-6">
+                            <div class="font-semibold text-gray-900">
+                                <p>
+                                    <span class="absolute inset-0" />
+                                    {{ __("attachs.fourth") }}
+                                </p>
+                            </div>
+                            <p class="text-blue-500 underline cursor-pointer">
+                                {{ __("attachs.download") }}
                             </p>
                         </div>
                     </a>
@@ -50,30 +121,4 @@
 
 <script setup>
 import fileImg from "../../img/file.svg";
-const attachs = [
-    {
-        id: 1,
-        name: "Положение регламет",
-        link: "/reglament.pdf",
-        imageUrl: fileImg,
-    },
-    {
-        id: 2,
-        name: "Публичная оферта",
-        link: "/reglament.pdf",
-        imageUrl: fileImg,
-    },
-    {
-        id: 3,
-        name: "Сертификат трассы",
-        link: "/reglament.pdf",
-        imageUrl: fileImg,
-    },
-    {
-        id: 3,
-        name: "Расписка о здоровье",
-        link: "/reglament.pdf",
-        imageUrl: fileImg,
-    },
-];
 </script>
