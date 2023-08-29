ee5238eac0f2e0f8fdbc8da8e1e10c34fec65fc2
Rasulov Akmal <akmal.rasulov@megafon.tj>
2023-08-29 17:45:13 +0500

    show banner on desktop

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col min-h-screen">
         <Header class="z-10" />
-        <!-- <Banner /> -->
+        <Banner class="hidden lg:block" />
         <div>
             <transition
                 enter-active-class="ease-out duration-200"
